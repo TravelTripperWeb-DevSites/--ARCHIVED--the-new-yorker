9093288c2b8590e92e76aec5c271c23d62baad6b
dmanikym <dmanikyam@traveltripper.com>
2017-01-06 20:32:52 +0530

    Fixes

--- js/scripts.js
+++ js/scripts.js
@@ -230,3 +230,18 @@ $('.modal .close').click(function () {
 	$('.modal iframe').removeAttr('src');
 });
 
+$(window).load(function(){
+  if( $(window).width() > 768 ) {
+
+     jQuery('ul.nav li.dropdown a').attr('data-toggle','disable');
+   //Add Hover effect to menus
+        jQuery('ul.nav li.dropdown').hover(function() {
+          jQuery(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn();
+        }, function() {
+          jQuery(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut();
+        });
+  }
+  else{
+	   //jQuery('ul.nav li.dropdown a.dropdown-toggle').removeAttr('data-toggle');
+  }
+});
