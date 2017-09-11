4c085b21acedb3f13b6f779fef42059c870055f6
Renu <rgunpal@traveltripper.com>
2017-09-11 12:26:56 +0530

    utm parameters

--- js/utm-tracking.js
+++ js/utm-tracking.js
@@ -19,12 +19,14 @@ function readCookie(name) {
     return null;
 }
 
+ // Parse the URL
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&amp;]" + name + "=([^&amp;#]*)"),
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
 // Give the URL parameters variable names
 var source = getParameterByName('utm_source');
 var medium = getParameterByName('utm_medium');
@@ -41,3 +43,40 @@ if(readCookie('utm_medium') == null || readCookie('utm_medium') == "") {
 if(readCookie('utm_campaign') == null || readCookie('utm_campaign') == "") {
     createCookie('utm_campaign', campaign);
 }
+
+
+$(document).ready(function(){
+        $(document).on("click", "a", function(){
+            var source = readCookie('utm_source');
+            var medium = readCookie('utm_medium');
+            var campaign = readCookie('utm_campaign');
+
+            if(source != null || source != ''){
+                var this_href = $(this).attr("href");
+                if(this_href.indexOf('newyorkerhotel.reztrip.com') != -1){
+                    this_href = this_href + '&utm_source='+source+ '&utm_medium='+medium+ '&utm_campaign='+campaign;
+                    $(this).attr('href',this_href);
+                }
+            }
+            
+
+        })
+        $('form').on('submit', function(){
+            var source = readCookie('utm_source');
+            var medium = readCookie('utm_medium');
+            var campaign = readCookie('utm_campaign');
+            if(source != null || source != ''){
+                var $utmEl = $("<input type='hidden' name='utm_source' value='" + source + "'><input type='hidden' name='utm_medium' value='" + medium + "'><input type='hidden' name='utm_campaign' value='" + campaign + "'>");
+                $(this).prepend($utmEl);
+            }
+        });
+
+        // Parse the URL
+        function getParameterByName(name) {
+            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                results = regex.exec(location.search);
+            return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        }
+})
+
